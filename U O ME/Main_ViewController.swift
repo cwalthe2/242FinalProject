d06e63ec0f2d2db25148559f2a3d605f2aa29b08
Collin Walther <cwalthe2@illinois.edu>
2016-11-24 15:36:45 -0600

    Added Facebook sign up/sign in functionality

--- U O ME/Main_ViewController.swift
+++ U O ME/Main_ViewController.swift
@@ -7,13 +7,15 @@
 //
 
 import UIKit
+import FBSDKLoginKit
 
-class Main_ViewController: UIViewController {
+class Main_ViewController: UIViewController, FBSDKLoginButtonDelegate {
     
     let MyKeychainWrapper = KeychainWrapper()
 
     @IBOutlet weak var loginButton: UIButton!
     
+    @IBOutlet weak var propicImageView: UIImageView!
     @IBOutlet weak var usernameField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
     @IBOutlet var swipeRecognizer: UISwipeGestureRecognizer!
@@ -21,20 +23,102 @@ class Main_ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
         
         
         if let storedUsername = UserDefaults.standard.value(forKey: "email") as? String {
             usernameField.text = storedUsername as String
         }
+        
+        let fbLoginButton = FBSDKLoginButton()
+        fbLoginButton.readPermissions = ["email", "public_profile"]
+        self.view.addSubview(fbLoginButton)
+        fbLoginButton.frame = CGRect(x: 79, y: 423, width: 162, height: 30)
+        
+        fbLoginButton.delegate = self
+        
+        if let token = FBSDKAccessToken.current() {
+            fetchProfile()
+        }
     }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        print("Did logout of facebook")
+    }
     
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        if error != nil{
+            print(error)
+            return
+        }
+        
+        fetchProfile()
+        print("Successful facebook login")
+    }
+    
+    func fetchProfile() {
+        print("Fetch profile")
+        
+        let parameters = ["fields": "id, email, first_name, last_name, picture.type(large)"]
+        FBSDKGraphRequest(graphPath: "/me", parameters: parameters)
+            .start(completionHandler:  { (connection, result, error) in
+                    let result = result as? NSDictionary
+                    let email = result?["email"] as? String
+                    let first_name = result?["first_name"] as? String
+                    let last_name = result?["last_name"] as? String
+                    let id = result?["id"]  as? String
+             
+                
+                print(email ?? "no email")
+                print(first_name ?? "no first name")
+                print(last_name ?? "no last name")
+                print(id ?? "no id")
+                
+                //self.propicImageView.image = self.getProfPic(fid: id!)
+                
+                if let profilePictureObj = result?.value(forKey: "picture") as? NSDictionary
+                {
+                    let data = profilePictureObj.value(forKey: "data") as! NSDictionary
+                    let pictureUrlString  = data.value(forKey: "url") as! String
+                    let pictureUrl = NSURL(string: pictureUrlString)
+                    
+
+                        
+                    let imageData = NSData(contentsOf: pictureUrl! as URL)
+                        
+                    if let imageData = imageData
+                    {
+                        let userProfileImage = UIImage(data: imageData as Data)
+                        self.propicImageView.image = userProfileImage
+
+                    }
+                    
+                    
+                }
+                
+            })
+    }
+    
+    func getProfPic(fid: String) -> UIImage? {
+        if (fid != "") {
+            let imgURLString = "http://graph.facebook.com/" + fid + "/picture?type=large" //type=normal
+            let imgURL = NSURL(string: imgURLString)
+            let imageData = NSData(contentsOf: imgURL! as URL)
+            let image = UIImage(data: imageData! as Data)
+            return image
+        }
+        return nil
+    }
+    
+
 
     func handleSwipe(_ sender:UISwipeGestureRecognizer){
         if (sender.direction == .left){
